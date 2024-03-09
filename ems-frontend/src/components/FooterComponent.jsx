2655ccfe4952e331c7c83c3cdd09b97712cef6c6
gauravsars <gauravsars11@gmail.com>
2024-03-10 00:57:32 +0530

    Commit-06: Adding Header and Footer Component jsx file.

---
+++ ems-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className = 'footer'>
+            <span>
+                All Rights Reserved 2024 
+            </span>
+        </footer>
+        
+    </div>
+  )
+}
+export { FooterComponent as default };
---
+++ ems-frontend/src/components/HeaderComponent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className='navbar navbar-dark bg-dark'>
+                <a className = "navbar-brand" href ="https://www.javaguides.com">Employee Management System</a>
+            </nav>
+
+        </header>
+
+
+    </div>
+  )
+}
+
+export { HeaderComponent as default };
