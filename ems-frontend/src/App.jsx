55a755b8a377ab9921572537411443f000dcbac2
gauravsars <gauravsars11@gmail.com>
2024-03-10 17:04:14 +0530

    Commit-008 : Adding a new Component EmployeeComponent. Also Routing to EmployeeComponent when the user clicks on Add Employee button.

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import HelloWorld from './HelloWorld'
 import FooterComponent from './components/FooterComponent'
 import HeaderComponent from './components/HeaderComponent'
+import EmployeeComponent from './components/EmployeeComponent'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
 import {BrowserRouter , Routes, Route } from 'react-router-dom'
 
@@ -17,6 +18,7 @@ function App() {
             <Route path = '/' element = {<ListEmployeeComponent/>}></Route>
             {/* // http://localhost:3000/employees */}
             <Route path = '/employees' element = {<ListEmployeeComponent/>}></Route>
+            <Route path = '/add-employee' element = {<EmployeeComponent/>}></Route>
             </Routes>
         <FooterComponent/>
     </BrowserRouter>
